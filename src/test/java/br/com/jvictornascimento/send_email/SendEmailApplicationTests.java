748111fe7b1aefa260e34cfbd04de22d46f9b210
jvictornascimento <victojoao789@gmail.com>
2024-07-09 19:49:02 -0300

    init project

---
+++ src/test/java/br/com/jvictornascimento/send_email/SendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jvictornascimento.send_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
